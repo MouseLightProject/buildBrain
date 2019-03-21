ea346b34429cfde36ff380667bb7c6115f26cd6d
Adam L. Taylor <taylora@janelia.hhmi.org>
2019-03-21 10:54:23 -0400

    Commented out unused code in buildBrain_workflow1.m

--- buildBrain_workflow1.m
+++ buildBrain_workflow1.m
@@ -1,4 +1,4 @@
-function [outputArgs] = buildBrain_workflow1(configfile)
+function buildBrain_workflow1(configfile)
 %BUILDBRAIN Aggregrates skeletonization results and build connectivity
 %graph of the brain
 %
@@ -32,11 +32,11 @@ if ~isfield(opt,'sampling')
 end
 myh5 = opt.inputh5;
 myh5prob = opt.h5prob;
-[~,name] = fileparts(myh5);
+%[~,name] = fileparts(myh5);
 %h5infofile = fullfile('./h5infos',['h5inf_',name,'.mat']);
 %mkdir(fileparts(h5infofile))
 
-[brainSize,RR,chunk_dims,rank] = h5parser(myh5,myh5prob);
+[brainSize,~,~,~] = h5parser(myh5,myh5prob);
 %%
 origin = h5read(opt.inputh5,[opt.h5prob,'_props/origin']);
 spacing = h5read(opt.inputh5,[opt.h5prob,'_props/spacing']);
@@ -63,99 +63,99 @@ if ~exist(frags_folder_path, 'file') ,
     mkdir(frags_folder_path) ;
 end
 %%
-[subs,edges,A,weights] = skel2graph(opt);
-subs_ori = subs;
-edges_ori = edges;
-weights_ori = weights;
-A_ori = A;
+[subs,~,A,~] = skel2graph(opt);
+%subs_ori = subs;
+%edges_ori = edges;
+%weights_ori = weights;
+%A_ori = A;
 %%
-subs = subs_ori;
-edges = edges_ori;
-weights = weights_ori;
-A = A_ori;
+%subs = subs_ori;
+%edges = edges_ori;
+%weights = weights_ori;
+%A = A_ori;
 %%
-maskids = []
+%maskids = []
 % maskids = [56 672 1022 1031]; %ACB/Caudoputamen/Globus pallidus, external segment/Globus pallidus, internal segment
 %maskids = [956 844 882 686 56 1022 1031 1021 1085 719 882 583 182305705 182305709 182305713]
-gt_swcfolder = '/nrs/mouselight/seggui/swcfiles/GT/2017-09-25_striatum_neurons_temp'
-if ~isempty(maskids)
-    %%
-    [hits_allen_brain] = maskWithAllenAtlas(params,subs_ori,maskids);
-    if exist('gt_swcfolder','var')
-        addpath(genpath('./scripts'))
-        [hits_gt,hits_delete,swcout,gtfile] = cropSectionBasedOnGT(params,gt_swcfolder,subs);
-        % keep allen_brain and gt then substract delete
-        keepthese = union(setdiff(hits_allen_brain,hits_delete),hits_gt);
-        figure, myplot3(subs(keepthese,:),'.')
-    else
-        keepthese = hits_allen_brain;
-    end
-    %%
-    subs = subs(keepthese,:);
-    A = A(keepthese,:);
-    A = A(:,keepthese);
-end
+%gt_swcfolder = '/nrs/mouselight/seggui/swcfiles/GT/2017-09-25_striatum_neurons_temp'
+% if ~isempty(maskids)
+%     %%
+%     [hits_allen_brain] = maskWithAllenAtlas(params,subs_ori,maskids);
+%     if exist('gt_swcfolder','var')
+%         addpath(genpath('./scripts'))
+%         [hits_gt,hits_delete,swcout,gtfile] = cropSectionBasedOnGT(params,gt_swcfolder,subs);
+%         % keep allen_brain and gt then substract delete
+%         keepthese = union(setdiff(hits_allen_brain,hits_delete),hits_gt);
+%         figure, myplot3(subs(keepthese,:),'.')
+%     else
+%         keepthese = hits_allen_brain;
+%     end
+%     %%
+%     subs = subs(keepthese,:);
+%     A = A(keepthese,:);
+%     A = A(:,keepthese);
+% end
 %
 % delete junks due to ventricle
-[S,Comps] = graphconncomp(A,'DIRECTED',false);
-Y = histcounts(Comps,1:S+1);
-if 0
-    junk_locs = [[73336.7, 14360.0, 34718.8];
-    [71850.7, 14436.3, 34497.8];
-    [71870.7, 14308.7, 34508.5];
-    [73264.9, 14183.1, 34508.5];
-    [73328.9, 14801.0, 34508.5];
-    [71817.1, 14684.7, 34508.5];
-    [71875.3, 14339.0, 34448.2];
-    [73254.4, 14099.3, 34436.1];
-    [73366.1, 14453.1, 34753.0];
-    ];
-end
-if exist('junk_locs','var')
-    figure(100)
-    cla
-    ic_ids_all = zeros(1,length(Comps));
-    for ilocs = 1:size(junk_locs,1)
-        [aa,bb] = min(sum(abs(subs-um2pix(params,junk_locs(ilocs,:))),2));
-        ic_ids=Comps==Comps(bb);
-        ic_ids_all = ic_ids_all|ic_ids;
-        hold on
-        subs_ = subs(ic_ids,:);
-        A_ = A(ic_ids,:); A_ = A_(:,ic_ids);
-        gplot3(A_,subs_,'-');
-    end
-    keepthese = find(~ic_ids_all);
-    subs = subs(keepthese,:);
-    A = A(keepthese,:);
-    A = A(:,keepthese);
-end
+%[~,Comps] = graphconncomp(A,'DIRECTED',false);
+%Y = histcounts(Comps,1:S+1);
+% if 0
+%     junk_locs = [[73336.7, 14360.0, 34718.8];
+%     [71850.7, 14436.3, 34497.8];
+%     [71870.7, 14308.7, 34508.5];
+%     [73264.9, 14183.1, 34508.5];
+%     [73328.9, 14801.0, 34508.5];
+%     [71817.1, 14684.7, 34508.5];
+%     [71875.3, 14339.0, 34448.2];
+%     [73254.4, 14099.3, 34436.1];
+%     [73366.1, 14453.1, 34753.0];
+%     ];
+% end
+% if exist('junk_locs','var')
+%     figure(100)
+%     cla
+%     ic_ids_all = zeros(1,length(Comps));
+%     for ilocs = 1:size(junk_locs,1)
+%         [aa,bb] = min(sum(abs(subs-um2pix(params,junk_locs(ilocs,:))),2));
+%         ic_ids=Comps==Comps(bb);
+%         ic_ids_all = ic_ids_all|ic_ids;
+%         hold on
+%         subs_ = subs(ic_ids,:);
+%         A_ = A(ic_ids,:); A_ = A_(:,ic_ids);
+%         gplot3(A_,subs_,'-');
+%     end
+%     keepthese = find(~ic_ids_all);
+%     subs = subs(keepthese,:);
+%     A = A(keepthese,:);
+%     A = A(:,keepthese);
+% end
 %%
 % junk_ids = junk_ids_070218sample();
-junk_ids = [];
-if exist('junk_ids','var')
-    %%
-    if opt.viz
-        figure(100)
-        cla
-        for ilocs = 1:size(junk_ids,1)
-            ic_ids=Comps==junk_ids(ilocs);
-            hold on
-            subs_ = subs(ic_ids,:);
-            A_ = A(ic_ids,:); A_ = A_(:,ic_ids);
-            gplot3(A_,subs_,'-');
-        end
-    end
-    ic_ids_all = zeros(1,length(Comps));
-    for ilocs = 1:size(junk_ids,1)
-        ic_ids=Comps==junk_ids(ilocs);
-        ic_ids_all = ic_ids_all|ic_ids;
-    end
-    %%
-    keepthese = find(~ic_ids_all);
-    subs = subs(keepthese,:);
-    A = A(keepthese,:);
-    A = A(:,keepthese);
-end
+% junk_ids = [];
+% if exist('junk_ids','var') && ~isempty(junk_ids) ,
+%     %%
+%     if opt.viz
+%         figure(100)
+%         cla
+%         for ilocs = 1:size(junk_ids,1)
+%             ic_ids=Comps==junk_ids(ilocs);
+%             hold on
+%             subs_ = subs(ic_ids,:);
+%             A_ = A(ic_ids,:); A_ = A_(:,ic_ids);
+%             gplot3(A_,subs_,'-');
+%         end
+%     end
+%     ic_ids_all = zeros(1,length(Comps));
+%     for ilocs = 1:size(junk_ids,1)
+%         ic_ids=Comps==junk_ids(ilocs);
+%         ic_ids_all = ic_ids_all|ic_ids;
+%     end
+%     %%
+%     keepthese = find(~ic_ids_all);
+%     subs = subs(keepthese,:);
+%     A = A(keepthese,:);
+%     A = A(:,keepthese);
+% end
 
 %%
 % [A,subs] = filterEdges(A,subs,params)
