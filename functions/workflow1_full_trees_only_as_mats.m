a7acc28c44e530e83604d1503fefebb0d9186c45
Adam L. Taylor <taylora@janelia.hhmi.org>
2019-08-07 16:06:34 -0400

    Split into a serial for loop for big components, then a parfor loop for small

--- functions/workflow1_full_trees_only_as_mats.m
+++ functions/workflow1_full_trees_only_as_mats.m
@@ -64,113 +64,42 @@ function workflow1_full_trees_only_as_mats(G, subs, options)
         mkdir(output_folder_path) ;
     end
 
+    big_component_threshold = 1e6 ;
+    big_component_count = sum(size_from_component_id>=big_component_threshold) ;
+        
     % The main loop
     fprintf('Starting the big parfor loop, going to process %d components...\n', component_count) ;
-    did_discard = false(component_count, 1) ;
+    %did_discard = false(component_count, 1) ;
     parfor_progress(components_to_process_count) ;
-    for component_id = 1 : component_count ,
-        % Extract this component
+    % Do the big ones in a regular for loop, since each requires a lot of
+    % memory
+    for component_id = 1 : big_component_count ,
+        % Process this component
         component = component_from_component_id{component_id} ;
-        component_size = size_from_component_id(component_id) ;
-        
-        % Output some info
-        fprintf('Processing component with id %d, size is %d nodes...\n', component_id, component_size) ;
-        
-        % Check if too small, skip iter if so
-        if component_size<=size_threshold ,
-            % Update the progress bar
-            did_discard(component_id) = true ;
-            parfor_progress() ;
-            continue
-        end                   
-        
-        % Check if the file already exists, skip iter if so
-        tree_mat_file_name = sprintf('auto-cc-%06d.mat', component_id) ;
-        tree_mat_file_path = fullfile(output_folder_path, tree_mat_file_name);
-        if exist(tree_mat_file_path, 'file') ,
-            % Update the progress bar
-            parfor_progress() ;
-            continue
-        end
-        
-        % Get the graph for this component
-        subs_for_component = subs(component,:) ;
-        G_for_component = G.subgraph(component) ;
-        A_for_component = G_for_component.adjacency ;
-
-        % Do something
-        a_leaf_node_id = find(sum(A_for_component)==1, 1) ;
-        [eout] = graphfuncs.buildgraph(A_for_component, a_leaf_node_id) ;
-        inupdate.dA = sparse(eout(:,1),eout(:,2),1,component_size,component_size);
-        inupdate.D = ones(component_size,1);
-        inupdate.R = ones(component_size,1);
-        inupdate.X = subs_for_component(:,1);
-        inupdate.Y = subs_for_component(:,2);
-        inupdate.Z = subs_for_component(:,3);
-        %%
-        deleteThese = NaN;
-        while ~isempty(deleteThese) ,
-            [inupdate, deleteThese] = prunTree(inupdate, length_threshold, voxres) ;
-            if do_visualize
-                hold on
-                gplot3(inupdate.dA,[inupdate.X,inupdate.Y,inupdate.Z]);
-                drawnow
-            end
-        end
-        %%
-        % shuffle root to one of the leafs for efficiency and not
-        % splitting long stretches into half
-        if size(inupdate.dA,1)>1
-            [eoutprun] = graphfuncs.buildgraph(inupdate.dA);
-            component_size = max(eoutprun(:));
-            inupdate.dA = sparse(eoutprun(:,1),eoutprun(:,2),1,component_size,component_size);
-        else
-        end
-        if do_visualize
-            hold on
-            gplot3(inupdate.dA,[inupdate.X,inupdate.Y,inupdate.Z],'LineWidth',3);
-            drawnow
-        end
-        %%
-        if length(inupdate.dA)<size_threshold
-            %fprintf('Component with id %d fails to meet the size threshold after pruning, so discarding.\n', component_id) ;
-            did_discard(component_id) = true ;
-            continue
-        end
-        %%
-        inupdate_smoothed = smoothtree(inupdate,options);
-        %%
-        % [L,list] = getBranches(inupdate_smoothed.dA);
-    %     if 0
-    %         outtree = inupdate_smoothed;
-    %     else
-            % outtree_old = downSampleTree(inupdate_smoothed,opt);
-        outtree_in_voxels = sampleTree(inupdate_smoothed, options) ;
-        outtree_in_voxels.units = 'voxels' ;
-        if do_visualize
-            cla
-            gplot3(inupdate_smoothed.dA,[inupdate_smoothed.X,inupdate_smoothed.Y,inupdate_smoothed.Z],'LineWidth',3);
-            hold on
-            gplot3(outtree_in_voxels.dA,[outtree_in_voxels.X,outtree_in_voxels.Y,outtree_in_voxels.Z],'--','LineWidth',3);
-            drawnow
-        end        
-        
-        % Convert centerpoint from voxel coords to um
-        outtree = convert_centerpoint_units_to_um(outtree_in_voxels, origin_in_nm, spacing_in_nm) ;
-                
-        % Write full tree as a .mat file
-        save_tree_as_mat(tree_mat_file_path, component_id, outtree) ;
+        process_single_component(component_id, component, G, subs, size_threshold, length_threshold, do_visualize, origin_in_nm, spacing_in_nm, voxres) ;
         
         % Update the progress bar
         parfor_progress() ;
     end
-    parfor_progress(0) ;
-    discarded_components_count = sum(did_discard) ;
-    fprintf('Of %d processed components, %d were discarded b/c they were too small after pruning.\n', ...
-            component_count, ...
-            discarded_components_count) ;
-    if components_to_process_count == discarded_components_count ,
-        fprintf('This means you don''t need to run this function again --- all trees have been generated.\n')
+
+    % Do the small ones in a parfor loop, since memory is less of an issue
+    % for them
+    parfor component_id = big_component_count+1 : component_count ,
+        % Process this component
+        component = component_from_component_id{component_id} ;
+        process_single_component(component_id, component, G, subs, size_threshold, length_threshold, do_visualize, origin_in_nm, spacing_in_nm, voxres) ;
+        
+        % Update the progress bar
+        parfor_progress() ;
     end
+    
+    parfor_progress(0) ;
+%     discarded_components_count = sum(did_discard) ;
+%     fprintf('Of %d processed components, %d were discarded b/c they were too small after pruning.\n', ...
+%             component_count, ...
+%             discarded_components_count) ;
+%     if components_to_process_count == discarded_components_count ,
+%         fprintf('This means you don''t need to run this function again --- all trees have been generated.\n')
+%     end
     toc(runtic) ;
 end
---
+++ process_single_component.m
@@ -0,0 +1,85 @@
+function process_single_component(component_id, component, G, subs, size_threshold, length_threshold, do_visualize, origin_in_nm, spacing_in_nm, voxres)
+    component_size= length(component) ;
+    
+    % Output some info
+    fprintf('Processing component with id %d, size is %d nodes...\n', component_id, component_size) ;
+
+    % Check if too small, return if so
+    if component_size<=size_threshold ,
+        return
+    end                   
+
+    % Check if the file already exists, return if so
+    tree_mat_file_name = sprintf('auto-cc-%06d.mat', component_id) ;
+    tree_mat_file_path = fullfile(output_folder_path, tree_mat_file_name);
+    if exist(tree_mat_file_path, 'file') ,
+        return
+    end
+
+    % Get the graph for this component
+    subs_for_component = subs(component,:) ;
+    G_for_component = G.subgraph(component) ;
+    A_for_component = G_for_component.adjacency ;
+
+    % Do something
+    a_leaf_node_id = find(sum(A_for_component)==1, 1) ;
+    [eout] = graphfuncs.buildgraph(A_for_component, a_leaf_node_id) ;
+    inupdate.dA = sparse(eout(:,1),eout(:,2),1,component_size,component_size);
+    inupdate.D = ones(component_size,1);
+    inupdate.R = ones(component_size,1);
+    inupdate.X = subs_for_component(:,1);
+    inupdate.Y = subs_for_component(:,2);
+    inupdate.Z = subs_for_component(:,3);
+    %%
+    deleteThese = NaN;
+    while ~isempty(deleteThese) ,
+        [inupdate, deleteThese] = prunTree(inupdate, length_threshold, voxres) ;
+        if do_visualize
+            hold on
+            gplot3(inupdate.dA,[inupdate.X,inupdate.Y,inupdate.Z]);
+            drawnow
+        end
+    end
+    %%
+    % shuffle root to one of the leafs for efficiency and not
+    % splitting long stretches into half
+    if size(inupdate.dA,1)>1
+        [eoutprun] = graphfuncs.buildgraph(inupdate.dA);
+        component_size = max(eoutprun(:));
+        inupdate.dA = sparse(eoutprun(:,1),eoutprun(:,2),1,component_size,component_size);
+    else
+    end
+    if do_visualize
+        hold on
+        gplot3(inupdate.dA,[inupdate.X,inupdate.Y,inupdate.Z],'LineWidth',3);
+        drawnow
+    end
+    %%
+    if length(inupdate.dA)<size_threshold
+        %fprintf('Component with id %d fails to meet the size threshold after pruning, so discarding.\n', component_id) ;
+        return
+    end
+    %%
+    inupdate_smoothed = smoothtree(inupdate,options);
+    %%
+    % [L,list] = getBranches(inupdate_smoothed.dA);
+%     if 0
+%         outtree = inupdate_smoothed;
+%     else
+        % outtree_old = downSampleTree(inupdate_smoothed,opt);
+    outtree_in_voxels = sampleTree(inupdate_smoothed, options) ;
+    outtree_in_voxels.units = 'voxels' ;
+    if do_visualize
+        cla
+        gplot3(inupdate_smoothed.dA,[inupdate_smoothed.X,inupdate_smoothed.Y,inupdate_smoothed.Z],'LineWidth',3);
+        hold on
+        gplot3(outtree_in_voxels.dA,[outtree_in_voxels.X,outtree_in_voxels.Y,outtree_in_voxels.Z],'--','LineWidth',3);
+        drawnow
+    end        
+
+    % Convert centerpoint from voxel coords to um
+    outtree = convert_centerpoint_units_to_um(outtree_in_voxels, origin_in_nm, spacing_in_nm) ;
+
+    % Write full tree as a .mat file
+    save_tree_as_mat(tree_mat_file_path, component_id, outtree) ;
+end    
